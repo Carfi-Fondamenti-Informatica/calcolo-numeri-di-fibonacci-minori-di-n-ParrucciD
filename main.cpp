09c6e29cb1045e5faedd1a110ab23b2f0d08f115
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-11-14 11:06:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main () {
+    int n=0;
+    cin >> n;
+    int numero1=1;
+    int numero2=1;
+    int numero3=0;
+    if(n>=1){
+        cout<<numero1<<endl<<numero2<<endl;
+    }while(n>=numero3){
+        numero3=numero1+numero2;
+        numero1=numero2;
+        numero2=numero3;
+        if(n>=numero3){
+            cout<< numero3 << endl;
+        }
+    }
+
+    return 0;
 }
